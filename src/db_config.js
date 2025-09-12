e6c06f2e012eec5ad8bbdbc6e75d9d16549a37e3
AMANDA ROTHMANN PIOVESANA <04566835006@senacrs.edu.br>
2025-09-12 10:15:31 -0300

    teste 2

--- src/db_config.js
+++ src/db_config.js
@@ -5,29 +5,16 @@ const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'root',
-    database: 'projeto_herbario',
-    multipleStatements: false
+    database: 'projeto_herbario'
 });
 
 connection.connect((err) => {
     if (err) {
-        console.error('Erro conexão MySQL:', err);
+        console.error('Erro na conexão MySQL:', err);
         process.exit(1);
     } else {
         console.log('MySQL conectado!');
     }
 });
-
-async function getData() {
-    try {
-      const connection = await pool.getConnection(); // Obtém uma conexão do pool
-      const [rows] = await connection.execute('SELECT*FROM projeto_herbario'); // Executa a consulta
-      connection.release(); // Libera a conexão de volta para o pool
-      return rows;
-    } catch (error) {
-      console.error("Erro ao buscar dados:", error);
-      throw error;
-    }
-}
 
 module.exports = connection;
